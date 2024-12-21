c437af4d7d642936d05be7c2efa932697c5a97b1
Elaine Wicaksono <elainewicaksono@proton.me>
2024-12-21 10:19:45 +0000

    jdk: use temurin for java 17

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -4,5 +4,5 @@ set -e
 if [[ $TARGET_VERSION -eq 21 ]]; then
     git clone --branch jdk-21.0.1+12.1 --depth 1 https://github.com/adoptium/jdk21u openjdk-21
 else
-    git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    git clone --branch release --depth 1 https://github.com/adoptium/jdk17u openjdk-17
 fi
